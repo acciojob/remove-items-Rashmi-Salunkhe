2ae8d1a0782b9bb217dd7c12dfca00263ce1db60
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-07-23 10:16:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.getElementById('removeButton').addEventListener('click', () => {
+    const colorSelect = document.getElementById('colorSelect');
+    colorSelect.remove(colorSelect.selectedIndex);
+});
